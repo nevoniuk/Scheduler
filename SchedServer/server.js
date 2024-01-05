bf99c1aca5efad63892781f56c1caa55bff43997
nevoniuk <nevoniuk@purdue.edu>
2024-01-04 23:56:03 -0800

    first commit

---
+++ SchedServer/server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const port = 3002
+//middle ware file
+const user_model = require('./user')
+
+app.use(express.json())
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+  next();
+})
+
+app.post('/loginattempts', (req, res) => {
+  user_model.getUser(req.body)
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
+})
+
+app.post('/user', (req, res) => {
+  user_model.createUser(req.body)
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
+})
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
---
+++ SchedServer/user.js
@@ -0,0 +1,87 @@
+
+const { 
+	v4: uuidv4,
+  } = require('uuid');
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'newuser',
+  host: 'localhost',
+  database: 'scheduler_db',
+  password: 'root',
+  port: 5432,
+});
+
+//wont create a user if one already exists
+const createUser = (body) => {
+	return new Promise(function (resolve, reject) {
+	  const { name, email, password } = body;
+	  pool.query(
+		"INSERT INTO users (userid, email, password) VALUES ($1, $2, $3) RETURNING *",
+		[name, email, password],
+		(error, results) => {
+		  if (error) {
+			reject(error);
+		  }
+		  if (results && results.rows) {
+			resolve(
+			  `A new user has been added: ${JSON.stringify(results.rows[0])}`
+			);
+		  } else {
+			reject(new Error("No results found"));
+		  }
+		}
+	  );
+	});
+  };
+
+  const getUser = async (body) => { //Used for login, will find user, then post login attempt
+	try {
+	  return await new Promise(function (resolve, reject) {
+		
+		const {emailText, passwordText} = body; //need to be the same name as the values passed in react app
+		
+		if (!emailText || !passwordText) {
+			reject(new Error("email or password not given"));
+		}
+		pool.query("SELECT id FROM users WHERE email=($1) AND password=($2);", [emailText, passwordText],
+		 (error, results) => {
+		  if (error) {
+			reject(error);
+		  }
+		  if (results && results.rows && (results.rows[0] != undefined)) {
+			console.log(results.rows[0])
+			//id exists then create login token
+			var d = Date().indexOf('G');
+			var token = uuidv4();
+			pool.query(
+				"INSERT INTO loginattempts (token, email, date) VALUES ($1, $2, $3) RETURNING *",
+				
+				[token, emailText, Date().slice(0, d-1)],
+				(error, results) => {
+				  if (error) {
+					console.log("error" + error)
+					reject(error);
+				  }
+				  if (results && results.rows) {
+					resolve(token);
+				  } else {
+					reject(new Error("Could obtain token"));
+				  }
+				}
+			  );
+			
+		  } else {
+			reject(new Error("No results found"));
+		  }
+		});
+	  });
+	} catch (error_1) {
+	  console.error(error_1);
+	  throw new Error(error_1);
+	}
+  };
+
+  module.exports = {
+	getUser,
+	createUser
+  };
